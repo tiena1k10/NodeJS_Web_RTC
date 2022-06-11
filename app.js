1befaa431faecc8cc8bc7402ba3ee243baf1cef2
tiena1k10 <tiena1k10@gmail.com>
2022-06-11 23:29:33 +0700

    update

--- app.js
+++ app.js
@@ -30,14 +30,14 @@ const PORT = process.env.PORT || 3000;
 //hi
 
 // database connection
-const dbURI = 'mongodb+srv://tiena1k10:123456T@chatapp.do7ro.mongodb.net/CallVideoApp?retryWrites=true&w=majority'
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+const dbURI = 'mongodb+srv://tiena1k10:edVJU2pfSvBfrRmy@cluster.gakas.mongodb.net/ChatApp?retryWrites=true&w=majority'
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
   .then((result) => {
     var server = app.listen(PORT)
     var io = socketio(server)
     const socketioControl = require("./controllers/socketio");
     socketioControl(io);
-    console.log("Express server listening on port %d in %s mode", PORT, app.settings.env); 
+    console.log("Express server listening on port %d in %s mode", PORT, app.settings.env);
   }).catch((err) => console.log(err));
 
 // routes
